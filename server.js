ca41383f6f6893405056a0609c5926877b77c6d7
Akshayachella <30687100+Akshayachella@users.noreply.github.com>
2017-08-09 15:31:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content = {
+    title: 'Article One ! Akshaya C',
+    heading:'Article One',
+    date: '9 August 2017',
+    content:
+};
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
