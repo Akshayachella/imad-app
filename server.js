ca7952de45094160cec46e753fcc59844436ff4d
Akshayachella <30687100+Akshayachella@users.noreply.github.com>
2017-08-09 16:01:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ var articleOne = {
 };
 
 function createTemplate (data){
+ var title = data.title;
+ var heading=data.heading;
+ var date=data.date;
+ var content=data.content;
  var htmlTemplate = `
      <html>
         <head>
